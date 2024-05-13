2856fbe57ef6c8b5ac14828d72c3f8cdb66a763a
Pridefulsnow299 <xdaw467@gmail.com>
2024-05-12 20:31:10 -0600

    BASE DE DATOS SQL

---
+++ trabajo.sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+-- Servidor: 127.0.0.1
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `trabajo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleos`
+--
+
+CREATE TABLE `empleos` (
+  `id_empleos` int(11) NOT NULL,
+  `id_empresa` int(11) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `fecha_publicacion` date NOT NULL,
+  `salario` double NOT NULL,
+  `ubicacion` int(100) NOT NULL,
+  `requisitos` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `postulaciones`
+--
+
+CREATE TABLE `postulaciones` (
+  `id_postulaciones` int(11) NOT NULL,
+  `id_empleos` int(11) NOT NULL,
+  `id_candidatos` int(11) NOT NULL,
+  `fecha_postulacion` date NOT NULL,
+  `aceptada` varchar(100) NOT NULL,
+  `rechazada` varchar(100) NOT NULL,
+  `pendiente` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `us_candidato`
+--
+
+CREATE TABLE `us_candidato` (
+  `id_candidato` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `correo` varchar(20) NOT NULL,
+  `tel` int(11) NOT NULL,
+  `exp_laboral` varchar(20) NOT NULL,
+  `educacion` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `us_empresa`
+--
+
+CREATE TABLE `us_empresa` (
+  `id_empresa` int(11) NOT NULL,
+  `nombre_empresa` varchar(12) NOT NULL,
+  `ubicacion` varchar(50) NOT NULL,
+  `correo` varchar(12) NOT NULL,
+  `contrasena` varchar(10) NOT NULL,
+  `tel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `empleos`
+--
+ALTER TABLE `empleos`
+  ADD PRIMARY KEY (`id_empleos`);
+
+--
+-- Indices de la tabla `postulaciones`
+--
+ALTER TABLE `postulaciones`
+  ADD PRIMARY KEY (`id_postulaciones`);
+
+--
+-- Indices de la tabla `us_candidato`
+--
+ALTER TABLE `us_candidato`
+  ADD PRIMARY KEY (`id_candidato`);
+
+--
+-- Indices de la tabla `us_empresa`
+--
+ALTER TABLE `us_empresa`
+  ADD PRIMARY KEY (`id_empresa`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `empleos`
+--
+ALTER TABLE `empleos`
+  MODIFY `id_empleos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `postulaciones`
+--
+ALTER TABLE `postulaciones`
+  MODIFY `id_postulaciones` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `us_candidato`
+--
+ALTER TABLE `us_candidato`
+  MODIFY `id_candidato` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `us_empresa`
+--
+ALTER TABLE `us_empresa`
+  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
